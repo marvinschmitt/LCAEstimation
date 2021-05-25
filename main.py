0d0ab62b522b7040650c1b9d3844f6be05d1b2a2
marvinschmitt <mail@marvin-schmitt.de>
2021-05-25 19:26:01 +0200

    update paths

--- main.py
+++
@@ -1,8 +0,0 @@
-from generative_models import generate_lca
-import numpy as np
-
-p = np.array([0.3, 0.7])
-x_acc = np.array([0., 0.])
-d = generate_lca(10, p, x_acc, 0.3, 0.2, 0.2, 0.4)
-print(d)
-breakpoint()
