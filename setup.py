f0d456e900c335762e743e129f2eed3cc8e8eb60
marvinschmitt <35921281+marvinschmitt@users.noreply.github.com>
2021-05-26 19:59:29 +0200

    update gitignore and setup for windows

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+
 from Cython.Build import cythonize
-from distutils.extension import Extension
 import numpy as np
 
 
